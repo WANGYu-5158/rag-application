0cd7c8c4e3643f2c8d74f54ee5874f82b3e4a67f
Jasonly198 <1155215611@link.cuhk.edu.hk>
2025-02-28 22:37:28 +0800

    kdb and minnio

--- src/main/java/com/example/ragapplication/controller/KdbController.java
+++ src/main/java/com/example/ragapplication/controller/KdbController.java
@@ -3,6 +3,7 @@ package com.example.ragapplication.controller;
 import com.example.ragapplication.pojo.Knowledgedb;
 import com.example.ragapplication.service.KdbService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,7 +32,12 @@ public class KdbController {
     }
 
     @PostMapping("/add")
-    public int addDb(@RequestParam("dbname") String dbname){
+    public ResponseEntity<String> addDb(@RequestParam("dbname") String dbname){
         return kdbService.addDb(dbname);
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<String> deleteDb(@RequestParam("dbId") int dbId){
+        return kdbService.deleteDb(dbId);
     }
 }
